233fd7e4c59e35a24aa3f484cc9a3286966c0e9d
Janis Leicis <just4fan.jl@gmail.com>
2018-04-09 23:51:04 +0300

    second day

---
+++ gallery-app/src/app/gallery/gallery.component.ts
@@ -0,0 +1,17 @@
+
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor(private ImageService: ImageService) { }
+
+  ngOnInit() {
+  }
+
+}
