278daecaacd361c1464825094b809b9122b04c74
Janis Leicis <just4fan.jl@gmail.com>
2018-04-12 01:24:37 +0300

    4 day

--- gallery-app/src/app/gallery/gallery.component.ts
+++ gallery-app/src/app/gallery/gallery.component.ts
@@ -1,18 +1,23 @@
 import { GalleryImage } from './../models/galleryimage.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { ImageService } from '../services/image.service';
 import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, OnChanges {
   images: Observable<GalleryImage[]>;
   constructor(private ImageService: ImageService) { }
 
   ngOnInit() {
+    this.images = this.ImageService.getImages();
+  }
+
+  ngOnChanges() {
     this.images = this.ImageService.getImages();
   }
 
